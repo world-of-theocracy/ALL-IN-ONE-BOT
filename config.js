8b3dcbb175c6577daf19c08866ca359e157b4e90
world-of-theocracy <terracalamitas@gmail.com>
2025-06-08 22:16:28 -0500

    Uconfig.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '879873984466935848',
   status: {
     rotateDefault: [
       { name: 'Netflix', type: ActivityType.Watching },
@@ -11,6 +11,4 @@ module.exports = {
     ],
     songStatus: true
   },
-  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
-  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
 }
